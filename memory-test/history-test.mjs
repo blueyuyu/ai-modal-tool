c9caffc0910f231f7186e5ed139ada9d4598bf6c
blueyuyu <ublueyuyuyu@gmail.com>
2026-03-05 08:33:53 +0800

    历史记录测试

--- memory-test/history-test.mjs
+++ memory-test/history-test.mjs
@@ -21,6 +21,37 @@ async function inMemoryDemo() {
   // 第一轮对话
   console.log("[第一轮对话]");
   const userMessage1 = new HumanMessage("你今天吃的什么？");
+  await history.addMessage(userMessage1);
+
+  const messages1 = [systemMessage, ...(await history.getMessages())];
+  const response1 = await model.invoke(messages1);
+  await history.addMessage(response1);
+
+  // 第二轮对话
+  const userMessage = new HumanMessage(
+    "好吃吗"
+  );
+
+  await history.addMessage(userMessage);
+
+  const messages2 = [systemMessage, ...await history.getMessages()];
+  const response2 = await model.invoke(messages2);
+
+  await history.addMessage(response2);
+
+  console.log('userMessage', userMessage.content);
+  console.log('response2', response2.content);
+
+  // 展示所有历史消息
+
+  const allMessage = await history.getMessages();
+  console.log("allMessage", allMessage);
+
+  allMessage.forEach((msg, index) => {
+    console.log(`第${index + 1}轮对话`);
+    console.log("------");
+    console.log(index + 1, 轮, "------", msg.type === "human" ? '用户' : 'AI', msg.content);
+  })
 }
 
 inMemoryDemo();
